0ddcc2142f7d48c26d04053cf28499f12d0dc615
Karen <kngs@ic.ufal.br>
2020-07-08 19:34:02 -0300

    feat: 🎸 update heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,10 @@ import { Hero } from './hero';
 export class HeroService {
   private heroesUrl = 'api/heroes'; // URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -32,6 +36,17 @@ export class HeroService {
         tap(
           (_) => this.log(`fetched hero by id ${id}`),
           catchError(this.handleError(`getHero by id ${id}`))
+        )
+      );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http
+      .put(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap(
+          (_) => this.log(`updated hero by id ${hero.id}`),
+          catchError(this.handleError('updatedHero'))
         )
       );
   }
