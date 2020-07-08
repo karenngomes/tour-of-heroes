eb73da8730704c1577fb602bebbd9e34b703c603
Karen <kngs@ic.ufal.br>
2020-07-08 14:42:40 -0300

    feat: 🎸 add a hero detail route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from './../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,22 +10,11 @@ import { MessageService } from './../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   getHeroes() {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
-
-  onSelect(hero: Hero) {
-    this.messageService.add(
-      `HeroesComponent: Selected hero with id ${hero.id}`
-    );
-    this.selectedHero = hero;
   }
 
   ngOnInit(): void {
